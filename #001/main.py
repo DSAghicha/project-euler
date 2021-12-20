2e2c5c50559aca861b1e4edd7d532747b0421bfd
DSAghicha <dsaghicha211@gmail.com>
2021-12-21 02:16:47 +0530

    Final Solution

--- #001/main.py
+++ #001/main.py
@@ -1,8 +1,16 @@
+"""
+@param: n -> int : Upper Limit of the range
+"""
 def multiples(n: int) -> int:
-    pass
+    num: list = []
+    for i in range(1, n):
+        if (i % 3 == 0) or (i % 5 == 0):
+            num.append(i)
+    return sum(num)
 
 
 if __name__ == '__main__':
     t: int = int(input())
     for _x in range(t):
         n: int = int(input())
+        print(multiples(n))
